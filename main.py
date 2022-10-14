808772aad00e30c77d3912f8311001ccf4a11f4f
Cesar Diaz <113930497+0Cesar0@users.noreply.github.com>
2022-10-14 17:29:02 +0000

    practice

--- main.py
+++ main.py
@@ -36,3 +36,25 @@ else:print("This number is odd")
 color = ["red","blue","violet","pink","black"]
 random_color = choice(color)
 print(f"color is {random_color}")
+
+# Random Practice #1
+  # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
+number = randint(1,11)
+print("That number.")
+print(number)
+  
+# Random Practice #2
+# Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
+num2 = randint(0,1)
+print(num2)
+print("This number.")
+# Random Practice #3
+# Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
+# names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+random_name = choice(names)
+print(f"They have been chosen.:{random_name}")
+
+
+  
+  
